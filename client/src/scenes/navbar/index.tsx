99082ee53dc31f5041b1e625b2f96f0bc5bf8c46
codeftsaikat <soikatdey18@gmail.com>
2024-12-06 10:25:45 -0800

    Includes layout and structure for the dashboard

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,17 +1,17 @@
 import PixIcon from "@mui/icons-material/Pix";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 type Props = {};
 
 const Navbar = (_props: Props) => {
   return (
-    <div className="text-gray-300 flex justify-between items-center gap-2">
+    <div className="text-gray-300 flex justify-between items-center gap-2 fixed z-10 px-[40px] py-4 w-full bg-opacity-30 bg-black">
       <div className="flex gap-2">
         <PixIcon/>
         <p className="text-[16px]">FinanceTracker</p>
       </div>
-      <div className="flex gap-2">
-        <NavLink className={({isActive})=>(isActive ? 'text-gray-200' : "text-gray-500")} to={"/"}>Dashboard</NavLink>
-        <NavLink className={({isActive})=>(isActive ? 'text-gray-200' : "text-gray-500")} to={"/prediction"}>Prediction</NavLink>
+      <div className="flex items-center gap-2">
+        <NavLink className={({isActive})=>(isActive ? 'text-gray-200 text-[16px]' : "text-gray-500")} to={"/"}>Dashboard</NavLink>
+        <NavLink className={({isActive})=>(isActive ? 'text-gray-200 text-[16px]' : "text-gray-500")} to={"/prediction"}>Prediction</NavLink>
       </div>
     </div>
   );
