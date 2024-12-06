1e9edfdbb0640e965469bdac19db075493d36a01
codeftsaikat <soikatdey18@gmail.com>
2024-12-06 04:26:47 -0800

    Updated navbar design and made it more impressive

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,62 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
+// import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+// export default tseslint.config(
+//   { ignores: ['dist'] },
+//   {
+//     extends: [js.configs.recommended, ...tseslint.configs.recommended],
+//     files: ['**/*.{ts,tsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//     },
+//     rules: {
+//       ...reactHooks.configs.recommended.rules,
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//     },
+//   },
+// )
+
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { configs as tsConfigs } from '@typescript-eslint/eslint-plugin'; // Correct import for TypeScript ESLint
+
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'], // Specify files/folders to ignore
+  },
+  {
+    files: ['**/*.{ts,tsx}'], // Apply this configuration to TypeScript files
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020, // ECMAScript version
+      globals: globals.browser, // Use browser globals
     },
+    extends: [
+      js.configs.recommended, // JavaScript recommended rules
+      ...tsConfigs.recommended, // TypeScript recommended rules
+    ],
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-hooks': reactHooks, // Add React Hooks plugin
+      'react-refresh': reactRefresh, // Add React Refresh plugin
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules, // Add React Hooks recommended rules
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }, // React Refresh rule configuration
       ],
     },
   },
-)
+];
